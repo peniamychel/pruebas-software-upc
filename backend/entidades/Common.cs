c865117a7a99b7bc35a39e2d8a6e0b16c992839f
maykad7 <mayk.d7@gmail.com>
2023-08-14 04:22:59 -0400

    backend 1

---
+++ backend/entidades/Common.cs
@@ -0,0 +1,9 @@
+//using System.Data;
+
+namespace backend.entidades{
+    public class Common{
+    public string UsuarioRegistro{get; set;}
+    public DateTime FechaRegistro{get; set;}
+    public int EstadoRegistro{get; set;}    
+    }
+}
---
+++ backend/entidades/usuarios.cs
@@ -0,0 +1,10 @@
+//using Microsoft.AspNetCore.Identity;
+
+namespace backend.entidades{
+    public class Usuarios : Common{
+    public int Id{ get; set;}
+    public string NOmbreCompleto{ get; set;}
+    public string UserName{get; set;}
+    public string Password{get; set;}
+    }
+}
